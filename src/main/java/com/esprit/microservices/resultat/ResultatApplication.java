b7f9a08122f60aa1075f2d14248dd2b89ad78daa
Nour Khammassi <nour.khamessi@alight.eu>
2024-06-25 19:50:50 +0100

    first commit

---
+++ src/main/java/com/esprit/microservices/resultat/ResultatApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.microservices.resultat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResultatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esprit/microservices/resultat/ResultatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esprit.microservices.resultat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResultatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
